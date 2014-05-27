51474a4d490bd517929ba0f20acfdd9e0696c168
aidma11 <aidma11@aidma-trining>
2014-05-27 10:18:28 +0900

    これは3日目のコミットです。

--- Test/src/test/TestServlet.java
+++ Test/src/test/TestServlet.java
@@ -27,7 +27,7 @@ public class TestServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
+		System.out.println("tururu");
 	}
 
 	/**
